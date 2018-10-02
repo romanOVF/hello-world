9347d6859827eff4fd34cb5b199aa6cdbf3ce140
romanOVF <rkovriginpost@google.com>
2018-10-02 19:48:26 +0500

    Add one string into Temp.java

--- Temp.java
+++ Temp.java
@@ -1,6 +1,7 @@
 
 public class Temp {
   public static void main ( String [] args ) {
+    System.out.println ( "the class Temp" );
     First instance_1 = new First ();
     instance_1.print ();
   }
