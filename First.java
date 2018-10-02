940445d4c6b854a13243512c8fdb21644dee8369
romanOVF <rkovriginpost@google.com>
2018-10-02 20:47:48 +0500

    into First.java changed string

--- First.java
+++ First.java
@@ -1,6 +1,6 @@
 public class First {
   public void print () {
     System.out.println ( "the class First" );
-    System.out.println ( "Hallo!" );
+    System.out.println ( "Hallo! Welt" );
   }
 }
